ae639432e06d30883a23f94e7d4482e933ee1926
tangxve <11183504@qq.com>
2021-12-29 20:01:44 +0800

    feat: update

---
+++ src/canvasUtils.ts
@@ -0,0 +1,7 @@
+import { CanvasBase } from './types'
+// 绘制直线
+// @ts-ignore
+
+export const drawDath = ({ canvas }: CanvasBase) => {
+  // canvas.getContext('2d')
+}
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface CanvasBase {
+  canvas: HTMLCanvasElement,
+  width?: string,
+  height?: string,
+}
