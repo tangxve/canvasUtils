38f08deba5ab5a7b8dd6b028fcff768ab6b56ea8
tangxve <11183504@qq.com>
2022-01-06 19:55:05 +0800

    feat: 完成柱状图绘制

--- examples/src/rect.js
+++ examples/src/rect.js
@@ -1,5 +1,3 @@
-import { drawfillRect } from '../../src/core/canvasUtils'
-
 const canvas = document.getElementById('myCanvas')
 const cUtil = canvasUtils.createInstance({ canvas })
 const { ctx } = cUtil
@@ -7,24 +5,23 @@ const { ctx } = cUtil
 // 提前设置属性
 const wd = canvas.clientWidth
 const ht = canvas.clientHeight
-const pad = 30
-const bottomPad = 30
+const pad = 20
+const bottomPad = 20
 const step = 80
 
 // 绘制坐标系
 cUtil.drawAxis({ wd, ht, pad, bottomPad, step })
-
 // 绘制柱状图
 ctx.beginPath()
-for (let i = 0; i < Math.floor(wd / step); i++) {
+for (let i = 1; i < Math.floor(wd / step); i++) {
   // 随机高度 区间 300 - 50
   const hegiht = Math.random() * 300 + 50
   const opt = {
-    x: 20 + i * step,
-    y: ht - bottomPad,
+    x: i * step,
+    y: ht - bottomPad - hegiht - 1,
     w: 40,
     h: hegiht,
-    fillStyle: 'orange'
+    fillStyle: cUtil.color16()
   }
   cUtil.drawfillRect(opt)
 }
